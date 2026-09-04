3def01755613b156e15460be718fad93a51fdc2c
Ankit Khatri KC <ankitkhatrik6@gmail.com>
2026-09-04 09:48:24 +0545

    feat(seo): implement robots.txt generation

---
+++ app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/api/'],
+    },
+    sitemap: 'https://tucoverify.ankitak.com.np/sitemap.xml',
+  }
+}
